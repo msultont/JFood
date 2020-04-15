0953f1352a510a86abfacacc8e1e8a5b1f7a1301
Muhammad Sulton Tauhid <msulton55@gmail.com>
2020-04-15 16:23:02 +0700

    modul8-01

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Customer"},{"p":"<Unnamed>","l":"DatabaseFood"},{"p":"<Unnamed>","l":"Food"},{"p":"<Unnamed>","l":"Invoice"},{"p":"<Unnamed>","l":"JFood"},{"p":"<Unnamed>","l":"Location"},{"p":"<Unnamed>","l":"Seller"}]
